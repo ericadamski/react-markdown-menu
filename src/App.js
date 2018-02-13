b4be38f4d7221f969f8760bf09b257dc28308e44
Eric Adamski <er.adamski@gmail.com>
2018-02-13 15:03:20 -0500

    WIP :warning:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { MediumMenu } from "react-markdown-menu";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    if (this.area) {
+      this.area.addEventListener("select", () => {
+        const { value, selectionStart, selectionEnd } = this.area;
+
+        this.setState({
+          selectionStart,
+          selectionEnd,
+          selection: value.substr(selectionStart, selectionEnd)
+        });
+      });
+    }
+  }
+
+  onChange(text) {
+    const { selectionStart, selectionEnd } = this.state;
+
+    this.area.setSelectionRange();
+    // this.area.value = this.area.value.slice(selectionStart, selectionEnd);
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +34,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <MediumMenu x={10} y={10} selection="hello" onChange={console.log} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <textarea ref={node => (this.area = node)} />
       </div>
     );
   }
